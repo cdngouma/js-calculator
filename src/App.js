f389786beb0fdbc2fd1b1219c8f69df27b49ca82
cdngouma <ngouma.chrys@gmail.com>
2020-07-30 13:42:30 -0400

    Implemented digits input event handler

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const appName = "Standard Calculator";
+const DIGIT_LIMIT = 15;
 
 function App() {
+   //const [formula, setFormula] = useState([]);
+   const [currentVal, setCurrentVal] = useState([]);
+   //const [result, setResult] = useState(0);
+   //const [lastEntered, setLastEntered] = useState(0);
+
+   const handleDigitInput = (event) => {
+      const digit = event.target.textContent;
+      const oldVal = currentVal;
+      
+      if (oldVal.length < DIGIT_LIMIT) {
+         setCurrentVal([...oldVal, digit]);
+      } else {
+         setCurrentVal(["Digit limit met"]);
+         setTimeout(() => setCurrentVal([...oldVal]), 1200);
+      }
+   }
+
+   const removeLastDigit = (event) => {
+      const newVal = [...currentVal];
+      newVal.pop();
+      setCurrentVal(newVal);
+   }
+
+   const clearDisplay = (event) => {
+      setCurrentVal([]);
+   }
+
+   const resetCalculator = () => {
+      setCurrentVal([]);
+      //setFormula([]);
+      //setResult(0);
+      //setLastEntered(0);
+   }
+
    return (
       <div className="App">
          <h1 id="app_name">{appName}</h1>
          <div id="display">
             <div><span id="formula-display">1 + (2 x 3)</span></div>
-            <div><span id="result-display">0</span></div>
+            <div><span id="result-display">{currentVal.join("") || 0}</span></div>
          </div>
+
          <div id="keyboard">
             {/* System buttons */}
-            <button id="clear" className="app__func-btn">CE</button>
-            <button id="reset" className="app__func-btn">C</button>
-            <button id="delete" className="app__func-btn">AC</button>
+            <button id="clear" className="app__func-btn" onClick={clearDisplay}>CE</button>
+            <button id="reset" className="app__func-btn" onClick={resetCalculator}>C</button>
+            <button id="delete" className="app__func-btn" onClick={removeLastDigit}>AC</button>
             {/* Basic operators */}
             <button id="add" className="app__func-btn">+</button>
             <button id="subtract" className="app__func-btn">-</button>
@@ -27,16 +63,16 @@ function App() {
             <button id="inv" className="app__func-btn">1/x</button>
             <button id="percent" className="app__func-btn">%</button>
             {/* digits buttons */}
-            <button id="zero">0</button>
-            <button id="one">1</button>
-            <button id="two">2</button>
-            <button id="three">3</button>
-            <button id="four">4</button>
-            <button id="five">5</button>
-            <button id="six">6</button>
-            <button id="seven">7</button>
-            <button id="eight">8</button>
-            <button id="nine">9</button>
+            <button id="zero" onClick={handleDigitInput}>0</button>
+            <button id="one" onClick={handleDigitInput}>1</button>
+            <button id="two" onClick={handleDigitInput}>2</button>
+            <button id="three" onClick={handleDigitInput}>3</button>
+            <button id="four" onClick={handleDigitInput}>4</button>
+            <button id="five" onClick={handleDigitInput}>5</button>
+            <button id="six" onClick={handleDigitInput}>6</button>
+            <button id="seven" onClick={handleDigitInput}>7</button>
+            <button id="eight" onClick={handleDigitInput}>8</button>
+            <button id="nine" onClick={handleDigitInput}>9</button>
             {/* other buttons */}
             <button id="decimal">.</button>
             <button id="sign-toggler">+/-</button>
