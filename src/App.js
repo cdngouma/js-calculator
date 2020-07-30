b2a1d55ce0357c392ccd4943e2b11fbf95c061bc
cdngouma <ngouma.chrys@gmail.com>
2020-07-30 16:32:26 -0400

    Improved layout

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
    const handleDigitInput = (event) => {
       const digit = event.target.textContent;
       const oldVal = currentVal;
-      
+
       if (oldVal.length < DIGIT_LIMIT) {
          setCurrentVal([...oldVal, digit]);
       } else {
-         setCurrentVal(["Digit limit met"]);
+         setCurrentVal(["{Digit limit met}"]);
          setTimeout(() => setCurrentVal([...oldVal]), 1200);
       }
    }
@@ -41,42 +41,44 @@ function App() {
 
    return (
       <div className="App">
-         <h1 id="app_name">{appName}</h1>
-         <div id="display">
-            <div><span id="formula-display">1 + (2 x 3)</span></div>
-            <div><span id="result-display">{currentVal.join("") || 0}</span></div>
-         </div>
+         <div id="calculator">
+            <h1 id="app_name">{appName}</h1>
+            <div id="display">
+               <span id="formula-display">1 + (2 x 3)</span>
+               <span id="result-display">{currentVal.join("") || 0}</span>
+            </div>
 
-         <div id="keyboard">
-            {/* System buttons */}
-            <button id="clear" className="app__func-btn" onClick={clearDisplay}>CE</button>
-            <button id="reset" className="app__func-btn" onClick={resetCalculator}>C</button>
-            <button id="delete" className="app__func-btn" onClick={removeLastDigit}>AC</button>
-            {/* Basic operators */}
-            <button id="add" className="app__func-btn">+</button>
-            <button id="subtract" className="app__func-btn">-</button>
-            <button id="multiply" className="app__func-btn">x</button>
-            <button id="divide" className="app__func-btn">/</button>
-            {/* Scientific functions */}
-            <button id="sqr" className="app__func-btn">sqr</button>
-            <button id="sqrt" className="app__func-btn">sqrt</button>
-            <button id="inv" className="app__func-btn">1/x</button>
-            <button id="percent" className="app__func-btn">%</button>
-            {/* digits buttons */}
-            <button id="zero" onClick={handleDigitInput}>0</button>
-            <button id="one" onClick={handleDigitInput}>1</button>
-            <button id="two" onClick={handleDigitInput}>2</button>
-            <button id="three" onClick={handleDigitInput}>3</button>
-            <button id="four" onClick={handleDigitInput}>4</button>
-            <button id="five" onClick={handleDigitInput}>5</button>
-            <button id="six" onClick={handleDigitInput}>6</button>
-            <button id="seven" onClick={handleDigitInput}>7</button>
-            <button id="eight" onClick={handleDigitInput}>8</button>
-            <button id="nine" onClick={handleDigitInput}>9</button>
-            {/* other buttons */}
-            <button id="decimal">.</button>
-            <button id="sign-toggler">+/-</button>
-            <button id="equals">=</button>
+            <div id="keyboard">
+               {/* System buttons */}
+               <button id="clear" className="app__func-btn" onClick={clearDisplay}>CE</button>
+               <button id="reset" className="app__func-btn" onClick={resetCalculator}>C</button>
+               <button id="delete" className="app__func-btn" onClick={removeLastDigit}>DEL</button>
+               {/* Basic operators */}
+               <button id="add" className="app__func-btn">+</button>
+               <button id="subtract" className="app__func-btn">-</button>
+               <button id="multiply" className="app__func-btn">x</button>
+               <button id="divide" className="app__func-btn">/</button>
+               {/* Scientific functions */}
+               <button id="sqr" className="app__func-btn">sqr</button>
+               <button id="sqrt" className="app__func-btn">sqrt</button>
+               <button id="inv" className="app__func-btn">1/x</button>
+               <button id="percent" className="app__func-btn">%</button>
+               {/* digits buttons */}
+               <button id="zero" onClick={handleDigitInput}>0</button>
+               <button id="one" onClick={handleDigitInput}>1</button>
+               <button id="two" onClick={handleDigitInput}>2</button>
+               <button id="three" onClick={handleDigitInput}>3</button>
+               <button id="four" onClick={handleDigitInput}>4</button>
+               <button id="five" onClick={handleDigitInput}>5</button>
+               <button id="six" onClick={handleDigitInput}>6</button>
+               <button id="seven" onClick={handleDigitInput}>7</button>
+               <button id="eight" onClick={handleDigitInput}>8</button>
+               <button id="nine" onClick={handleDigitInput}>9</button>
+               {/* other buttons */}
+               <button id="decimal">.</button>
+               <button id="sign-toggler">+/-</button>
+               <button id="equals">=</button>
+            </div>
          </div>
       </div>
    );
