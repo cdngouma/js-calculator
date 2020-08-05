5287261e18fce1a647da4ff6ba1578624f04fbec
cdngouma <ngouma.chrys@gmail.com>
2020-08-04 22:49:57 -0400

    Updates

---
+++ precache-manifest.63f19e4b0c8ea9b411c623ce5bdbe352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af5e47df81a5c4235d2f31ed21bed37",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "d4ad17be9bee7a276178",
+    "url": "/js-calculator/static/css/main.06295150.chunk.css"
+  },
+  {
+    "revision": "68313bffaf09e8c5bebe",
+    "url": "/js-calculator/static/js/2.9b820dc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-calculator/static/js/2.9b820dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ad17be9bee7a276178",
+    "url": "/js-calculator/static/js/main.452295c7.chunk.js"
+  },
+  {
+    "revision": "ffa3baed92fe0e1b75c4",
+    "url": "/js-calculator/static/js/runtime-main.ed0fd4cc.js"
+  }
+]);
